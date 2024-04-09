3e22243ca3a23cc76552cab1709b5b3ebad4c231
pg493 <pg493@njit.edu>
2024-04-08 20:49:36 -0400

    Task2

--- datafeed/server3.py
+++ datafeed/server3.py
@@ -257,13 +257,16 @@ class App(object):
     """ The trading game server application. """
 
     def __init__(self):
-        self._book_1 = dict()
-        self._book_2 = dict()
-        self._data_1 = order_book(read_csv(), self._book_1, 'ABC')
-        self._data_2 = order_book(read_csv(), self._book_2, 'DEF')
-        self._rt_start = datetime.now()
-        self._sim_start, _, _ = next(self._data_1)
-        self.read_10_first_lines()
+        try:
+            self._book_1 = dict()
+            self._book_2 = dict()
+            self._data_1 = order_book(read_csv(), self._book_1, 'ABC')
+            self._data_2 = order_book(read_csv(), self._book_2, 'DEF')
+            self._rt_start = datetime.now()
+            self._sim_start, _, _ = next(self._data_1)
+            self.read_10_first_lines()
+        except StopIteration:
+            exit(1)
 
     @property
     def _current_book_1(self):
